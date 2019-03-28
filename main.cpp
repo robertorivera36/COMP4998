1255b5115783d82b27eec049608b7e2d5289357c
George Perez <george.perez.marrero@gmail.com>
2019-03-28 12:25:18 -0400

    Add initial code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
+void lex(std::ifstream& input_source, std::ofstream& output_lex) {
+
+    enum Token { inicio, Final, Si, sino, finsi, Mientras, finmientras, Escribe };
+
+    char c;
+
+    Token token;
+
+    switch(token) {
+        case 1: {
+            std::cout << "Test\n";
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
 
 int main() {
-    // some amazing code
+
+    std::ifstream input_source;
+    std::ofstream output_lex;
+
+    input_source.open("source.txt");
+    output_lex.open("output.txt");
+
+    lex(input_source, output_lex);
+
+    input_source.close();
+    output_lex.close();
 }
