35f39956150f091f235f5c1f666617e7698536bb
George Perez <george.perez.marrero@gmail.com>
2019-04-01 14:16:17 -0400

    Send latest code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 #include <fstream>
 
 void menu() {
@@ -12,16 +13,32 @@ void menu() {
     std::cout << "0. Exit LEX\n";
 }
 
-void lex(std::ifstream& input_source, std::ofstream& output_lex) {}
+bool lex(char buffer[]) {
+
+	char keywords[32][20] = {"inicio","final","Si","finsi","sino","Mientras","finmientras",
+							"Escribe"};
+	int i, flag = 0;
+	
+	for(i = 0; i < 32; ++i){
+		if(strcmp(keywords[i], buffer) == 0){
+			return true;
+			break;
+		}
+	}
+	return false;
+}
 
 int main() {
 
     int text_case;
 
+    char ch, buffer[20], parentesisIzquierdo[] = "(", parentesisDerecho[] = ")", 
+    oprelacional[] = "<>", oparitmetico[] = "+-*/", puntocoma[] = ";", asignacion[] = "=";
+    int i, j = 0;
+
     std::ifstream input_source;
     std::ofstream output_lex;
 
-    do {
         menu();
         std::cin >> text_case;
         switch(text_case) {
@@ -58,11 +75,78 @@ int main() {
                 break;
             }
         }
-    } while (text_case != 0);
 
     output_lex.open("output.txt");
 
-    lex(input_source, output_lex);
+    while(!input_source.eof()) {
+        ch = input_source.get();
+
+        if (ch == '<') {
+            if (input_source.peek() == '=') {
+                std::cout << ch << "es <opRelacional>, MEI\n";
+            } else if (input_source.peek() == '>') {
+                std::cout << ch << " es <opRelacional>, DIF\n";
+            } else {
+                std::cout << ch << " es <opRelacional>, MEN\n";
+            }
+        }
+        if (ch == '>') {
+            if (input_source.peek() == '=') {
+                std::cout << ch << " es <opRelacional, MAI\n";
+            } else {
+                std::cout << ch << " es <opRelacional, MAY\n";
+            }
+        }
+
+        for (i = 0; i < 4; ++i) {
+            if (ch == oprelacional[i]) {
+                output_lex << ch << " es <opRelacional>\n";
+            }
+        }
+
+        for (i = 0; i < 4; ++i) {
+            if (ch == oparitmetico[i]) {
+                output_lex << ch << " es <opAritmetico>\n";
+            }
+        }
+
+         for (i = 0; i < 1; ++i) {
+            if (ch == parentesisIzquierdo[i]) {
+                output_lex << ch << " es <parentesisIzquierdo>\n";
+            }
+        }
+
+        for (i = 0; i < 1; ++i) {
+            if (ch == parentesisDerecho[i]) {
+                output_lex << ch << " es <parentesisDerecho>\n";
+            }
+        }
+
+        for (i = 0; i < 1; ++i) {
+            if (ch == asignacion[i]) {
+                output_lex << ch << " es <asignacion>\n";
+            }
+        }
+
+        for (i = 0; i < 1; ++i) {
+            if (ch == puntocoma[i]) {
+                output_lex << ch << " es <puntoComa>\n";
+            }
+        }
+
+        if (isalnum(ch)) {
+            buffer[j++] = ch;
+        } else if ((ch == ' ' || ch == '\n') && (j != 0)) {
+            buffer[j] = '\0';
+            j = 0;
+
+            if (lex(buffer)) {
+                output_lex << buffer << " es <palabraReservada>\n";
+            } else {
+                output_lex << buffer << " es <identificador>\n";
+            }
+        }
+    }
 
     input_source.close();
     output_lex.close();
